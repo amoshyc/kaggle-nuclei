45e1d25c91be8856ee5c429ae7d78c01f3cb6f99
amoshyc <amoshuangyc@gmail.com>
2018-02-05 12:48:41 +0800

    Fix

--- nuclei/unet/model.py
+++ nuclei/unet/model.py
@@ -12,39 +12,34 @@ class UNet(nn.Module):
     def __init__(self):
         super(UNet, self).__init__()
 
-        self.down_block_1 = self.__conv_block(1, 64)
+        self.down_block_1 = self.__conv_block(3, 64)
         self.down_block_2 = self.__conv_block(64, 128)
         self.down_block_3 = self.__conv_block(128, 256)
         self.down_block_4 = self.__conv_block(256, 512)
 
-        self.center_1 = self.__center_block(64)
-        self.center_2 = self.__center_block(128)
-        self.center_3 = self.__center_block(256)
-        self.center_4 = self.__center_block(512)
-        self.center_5 = self.__center_block(1024)
+        self.center_1 = self.__conv(64, 64, (1, 1), p=0)
+        self.center_2 = self.__conv(128, 128, (1, 1), p=0)
+        self.center_3 = self.__conv(256, 256, (1, 1), p=0)
+        self.center_4 = self.__conv(512, 512, (1, 1), p=0)
+        self.center_5 = self.__conv(1024, 1024, (1, 1), p=0)
 
         self.up_block_1 = self.__conv_block(128, 64)
         self.up_block_2 = self.__conv_block(256, 128)
         self.up_block_3 = self.__conv_block(512, 256)
         self.up_block_4 = self.__conv_block(1024, 512)
 
-        self.last = self.__conv(64, 2)
+        self.last = self.__conv(64, 2, (3, 3), p=1)
 
-    def __conv(self, in_c, out_c, k):
-        conv = nn.Conv2d(in_c, out_c, k, padding=1)
+    def __conv(self, in_c, out_c, k, p=1):
+        conv = nn.Conv2d(in_c, out_c, k, padding=p)
         init.xavier_normal(conv.weight, math.sqrt(2))
         return nn.Sequential(conv, nn.ReLU())
 
     def __conv_block(self, in_c, out_c):
         return nn.Sequential(
-            self.__conv(in_c, out_c, (3, 3)),
-            self.__conv(out_c, out_c, (3, 3)),
-            self.__conv(out_c, out_c, (3, 3))
-        )
-
-    def __center_block(self, c):
-        return nn.Sequential(
-            self.__conv(c, c, (1, 1))
+            self.__conv(in_c, out_c, (3, 3), p=1),
+            self.__conv(out_c, out_c, (3, 3), p=1),
+            self.__conv(out_c, out_c, (3, 3), p=1),
         )
 
     def forward(self, x):
@@ -72,12 +67,12 @@ class UNet(nn.Module):
         up1 = F.upsample(out2, scale_factor=2, mode='bilinear')
         out1 = self.up_block_1(T.cat((out1, up1), 1))
 
-        res = self.last(out1)
+        res = F.sigmoid(self.last(out1))
         return res
 
 
 def test_model():
     model = UNet()
-    inp = Variable(torch.zeros(10, 3, 512, 512), requires_grad=False)
+    inp = Variable(T.zeros(10, 3, 512, 512), requires_grad=False)
     out = model(inp)
     assert out.size() == (10, 2, 512, 512)
